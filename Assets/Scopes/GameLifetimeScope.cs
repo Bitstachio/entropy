bcfc79df6138c59f70f130439b58acc00caea110
BarbodH <habibi.k.barbod@gmail.com>
2026-04-09 19:40:25 -0400

    Refactor event channel registrations to use dictionary

--- Assets/Scopes/GameLifetimeScope.cs
+++ Assets/Scopes/GameLifetimeScope.cs
@@ -1,6 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Core.Enums;
-using Core.Events.Base;
 using Core.Events.Channels;
 using Core.ExtendedBehaviours;
 using VContainer;
@@ -10,32 +11,29 @@ namespace Scopes
 {
     public sealed class GameLifetimeScope : LifetimeScope
     {
+        private static readonly Dictionary<Type, GameEventType> EventChannelToType = new()
+        {
+            { typeof(GameOver), GameEventType.GameOver },
+            { typeof(ScoreUpdatedChannel), GameEventType.ScoreUpdated },
+            { typeof(RockDestroyed), GameEventType.RockDestroyed },
+            { typeof(RockHitObject), GameEventType.RockHitObject },
+        };
+
         protected override void Configure(IContainerBuilder builder)
         {
             //----- Feature Installers -----
 
             GetComponentsInChildren<Installer>().ToList().ForEach(i => i.Install(builder));
-            
+
             //----- Event Channels -----
             // While explicit registration `.As<T>` is generally preferred for clarity,
             // `.AsImplementedInterfaces()` is used here for brevity as all event channels
             // strictly implement the same `IEventPublisher` and `IEventListener` pair.
 
-            builder.Register<EventChannel, GameOver>(Lifetime.Singleton)
-                .AsImplementedInterfaces()
-                .Keyed(GameEventType.GameOver);
-
-            builder.Register<EventChannel<float>, ScoreUpdatedChannel>(Lifetime.Singleton)
-                .AsImplementedInterfaces()
-                .Keyed(GameEventType.ScoreUpdated);
-
-            builder.Register<EventChannel<float>, RockDestroyed>(Lifetime.Singleton)
-                .AsImplementedInterfaces()
-                .Keyed(GameEventType.RockDestroyed);
-
-            builder.Register<EventChannel<string>, RockHitObject>(Lifetime.Singleton)
-                .AsImplementedInterfaces()
-                .Keyed(GameEventType.RockHitObject);
+            EventChannelToType.ToList().ForEach(pair =>
+                builder.Register(pair.Key, Lifetime.Singleton)
+                    .AsImplementedInterfaces()
+                    .Keyed(pair.Value));
         }
     }
 }
